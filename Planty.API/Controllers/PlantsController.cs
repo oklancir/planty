33cc6771d41cf17bd9896aec63e7b75b3929bbb9
Ozren Klančir <ozrenklancir@gmail.com>
2020-08-02 23:31:06 +0200

    Add PlantsController

--- Planty.API/Controllers/PlantsController.cs
+++ Planty.API/Controllers/PlantsController.cs
@@ -1,7 +1,11 @@
 ﻿namespace Planty.API.Controllers
 {
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Planty.Business;
+    using Planty.Business.Models;
 
     [Route("api/[controller]")]
     [ApiController]
@@ -12,6 +16,13 @@
         public PlantsController(IPlantService plantService)
         {
             _plantService = plantService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Plant>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok(await _plantService.GetAllAsync());
         }
     }
 }
