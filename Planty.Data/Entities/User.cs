69c3fd4487d3d6fb1a1a23bdbd70229e8470b4a5
Ozren Klančir <ozrenklancir@gmail.com>
2020-08-12 21:33:31 +0200

    Update User entity

--- Planty.Data/Entities/User.cs
+++ Planty.Data/Entities/User.cs
@@ -9,6 +9,14 @@
 
         public DateTime CreatedAt { get; set; }
 
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
         public string Username { get; set; }
 
         public string PasswordHash { get; set; }
