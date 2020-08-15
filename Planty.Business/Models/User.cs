8016cb4a584552249cc48c0b856f0fbe08754829
Ozren Klančir <ozrenklancir@gmail.com>
2020-08-15 20:01:25 +0200

    Modify User model

--- Planty.Business/Models/User.cs
+++ Planty.Business/Models/User.cs
@@ -2,8 +2,14 @@
 {
     using System;
 
-    public class User : UserBase
+    public class User
     {
         public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
     }
 }
