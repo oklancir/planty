f0f8a6c9d56f222bd77bef5bd9ddf60a0821d551
Ozren Klančir <ozrenklancir@gmail.com>
2020-08-29 00:04:45 +0200

    Add UsersController and Cart and CartItem entities

---
+++ Planty.Data/Entities/Cart.cs
@@ -0,0 +1,17 @@
+﻿namespace Planty.Data.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using Planty.Data.Interfaces;
+
+    public class Cart : IEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public IEnumerable<CartItem> CartItems { get; set; }
+    }
+}
---
+++ Planty.Data/Entities/CartItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Planty.Data.Entities
+{
+    using System;
+    using Planty.Data.Interfaces;
+
+    public class CartItem : IEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public Guid CartId { get; set; }
+
+        public Guid ProductId { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
