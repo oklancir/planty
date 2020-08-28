ee2bfe12a05c8bc67a798d73c92882f0b5733a91
Ozren Klančir <ozrenklancir@gmail.com>
2020-08-28 13:14:27 +0200

    Remove UserNotFoundException, add generic NotFoundException for entities

---
+++ Planty.Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,34 @@
+﻿namespace Planty.Common.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class NotFoundException : Exception
+    {
+        public NotFoundException()
+            : base("Entity not found")
+        {
+        }
+
+        public NotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public NotFoundException(Type type)
+            : base($"Entity of type {type?.ToString()} not found")
+        {
+        }
+
+        public NotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected NotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
